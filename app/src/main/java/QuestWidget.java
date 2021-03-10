6a1ad6419a6e6ed57cb81ddfc447a2325d101d23
zjayee <75448434+zjayee@users.noreply.github.com>
2021-03-10 13:00:53 -0800

    inflate quest widget

--- app/src/main/java/QuestWidget.java
+++ app/src/main/java/QuestWidget.java
@@ -3,23 +3,36 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.example.innofuel.R;
 
+
+
 public class QuestWidget extends LinearLayout implements View.OnClickListener
 {
 
-    public QuestWidget(Context context, @Nullable AttributeSet attrs) {
+
+    public QuestWidget(Context context, @Nullable AttributeSet attrs, String questName, String questDescription, String questRewards, String completion) {
         super(context, attrs);
 
+        //inflate layout
         LayoutInflater inflater = LayoutInflater.from(context);
         inflater.inflate(R.layout.quest_widget,this, true);
 
+        TextView questNameTextview = findViewById(R.id.questName);
+        TextView questDescriptionTextview = findViewById(R.id.questDescription);
+        TextView questRewardsTextview = findViewById(R.id.rewardsText);
+        TextView completionTextview = findViewById(R.id.completionText);
+
+        questNameTextview.setText(questName);
+        questDescriptionTextview.setText(questDescription);
+        questRewardsTextview.setText(questRewards);
+        completionTextview.setText(completion);
+
         
-
-
     }
 
     @Override
