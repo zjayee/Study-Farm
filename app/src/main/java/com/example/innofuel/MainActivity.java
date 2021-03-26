c264461bae408928b6d9b918a4b723b9d4b4fee3
zjayee <75448434+zjayee@users.noreply.github.com>
2021-03-26 00:21:12 -0700

    timer screen layout

--- app/src/main/java/com/example/innofuel/MainActivity.java
+++ app/src/main/java/com/example/innofuel/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
 
 //        QuestWidget questWidget = new QuestWidget(this, null, "quest name", " sfdg"," sdge","3/10");
 //        LinearLayout linearLayout = findViewById(R.id.linearlayout);
