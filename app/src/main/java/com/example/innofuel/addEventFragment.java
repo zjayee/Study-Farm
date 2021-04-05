bf2ec86feb6eb335051ab0fe184c413d5d8f6076
zjayee <75448434+zjayee@users.noreply.github.com>
2021-04-05 00:47:44 -0700

    add event and add task fragment layout

--- app/src/main/java/com/example/innofuel/addEventFragment.java
+++ app/src/main/java/com/example/innofuel/addEventFragment.java
@@ -10,30 +10,24 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AddTaskFragment#newInstance} factory method to
+ * Use the {@link addEventFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AddTaskFragment extends Fragment {
+public class addEventFragment extends Fragment {
 
 
-
-
-
-    public AddTaskFragment() {
+    public addEventFragment() {
         // Required empty public constructor
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-
-     * @return A new instance of fragment AddTaskFragment.
+     * @return A new instance of fragment addEventFragment.
      */
-
-    public static AddTaskFragment newInstance() {
-        AddTaskFragment fragment = new AddTaskFragment();
-
+    // TODO: Rename and change types and number of parameters
+    public static addEventFragment newInstance() {
+        addEventFragment fragment = new addEventFragment();
         return fragment;
     }
 
@@ -42,6 +36,6 @@ public class AddTaskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_task, container, false);
+        return inflater.inflate(R.layout.fragment_add_event, container, false);
     }
 }
